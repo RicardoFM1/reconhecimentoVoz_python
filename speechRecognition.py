8a58dbc7559e2b7fb150ead8d7b575031fde800f
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2026-02-03 20:02:53 -0300

    Mais dinamico agora e melhorado

--- speechRecognition.py
+++ speechRecognition.py
@@ -8,17 +8,34 @@ with sr.Microphone() as source:
     print("Diga algo legal:")
     audio = reconhecimento.listen(source)
 
-try: 
-    texto = reconhecimento.recognize_google(audio, language='pt-BR')
+try:
+   
+    texto = reconhecimento.recognize_google(audio, language="pt-BR")
     print("Você disse:", texto)
+
+   
+    texto_pesquisa = texto.lower()
+
+    palavras_remover = ["google", "navegador", "youtube"]
+
+    for palavra in palavras_remover:
+        texto_pesquisa = texto_pesquisa.replace(palavra, "")
+
+    texto_pesquisa = texto_pesquisa.strip()
+
     if "navegador" in texto.lower() or "google" in texto.lower():
-        for i in range(3):
-            print("Abrindo navegador...")
-            time.sleep(1)
-        webbrowser.open('https://google.com')
-        print("Navegador aberto!")
-    elif "youtube" in texto.lower():
+        print("Abrindo navegador...")
         time.sleep(1)
-        webbrowser.open_new_tab("https://www.youtube.com")
+        webbrowser.open(
+            "https://google.com/search?q=" + texto_pesquisa
+        )
+
+    elif "youtube" in texto.lower():
+        print("Abrindo YouTube...")
+        time.sleep(1)
+        webbrowser.open_new_tab(
+            f"https://www.youtube.com/results?search_query={texto_pesquisa}"
+        )
+
 except sr.UnknownValueError:
     print("Não entendi o que quis dizer, tente novamente!")
